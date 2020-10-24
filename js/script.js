3ba5b1a088aa7c9dab8dacee0223e2c098dc0a72
Debi Suci Puspitasari <71931229+j3c118118@users.noreply.github.com>
2020-10-24 23:18:40 +0700

    Add files via upload
    UTS PABW

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {scrollFunction()};
+          
+  function scrollFunction() {
+    if (document.body.scrollTop > 680 || document.documentElement.scrollTop > 680) {
+      document.getElementById("logo").style.fontSize = "50px";
+    } else {
+      document.getElementById("logo").style.fontSize = "0px";
+    }
+  }
